e4f285b93070995645367759ea2395907a009bb7
Emil Fröjd <>
2018-11-02 11:27:57 +0100

    Added Init and Setup scripts for Master

---
+++ setupMaster.sh
@@ -0,0 +1,15 @@
+cd /home/ubuntu/
+
+git clone https://github.com/marcusfroling/CloudProject.git
+mkdir /home/ubuntu/naca_airfoil/geo_files/
+mkdir /home/ubuntu/naca_airfoil/msh_files/
+
+sudo chmod 755 -R code
+
+sudo service rabbitmq-server start
+
+sudo rabbitmqctl add_user none none
+sudo rabbitmqctl add_vhost nonevhost
+sudo rabbitmqctl set_permissions -p nonevhost none ".*" ".*" ".*"
+cd /home/ubuntu/CloudProject
+screen -d -m python run_airfoil_tasks.py
